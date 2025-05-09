b9370e50913db867692e1639d2b40416e3983f71
mgmhue <mghmue2000@gmail.com>
2025-05-09 22:18:02 +0630

    php day4

--- ex1.php
+++ ex1.php
@@ -14,7 +14,7 @@ if ($number % 2 !== 0){
 ?>
 
 
-  <!-- witch  -->
+<!-- //switch statement -->
 <?php
 $num1 = 10;
 $num2 = 5;
@@ -47,6 +47,84 @@ switch ($action) {
         }
         return $result;
     }
-    
+    echo "<br>";
     echo factorial(4); 
 ?>
+
+<!-- functiuon type in php --> 
+<?php
+//data type တွေသက်မှတ်လို့ရတယ် // ဘာData type နဲ့ရလာမှာလဲ // default value ထည့်လို့ရတယ်//
+function greeting($name = "Guest") {
+    return "Hello,$name";
+}
+$result = greeting();
+echo "<br>";
+echo $result;
+?>
+
+
+<!-- Arry -->
+<!-- Indexed Arry -->
+<!-- Associative Array -->
+<!-- Multidimensional Array -->
+<?php
+// Indexed Array
+$fruits = array("Apple", "Banana", "Cherry");
+echo $fruits[0];
+
+//foreach loop
+foreach ($fruits as $fruit) {
+    echo "<br>";
+    echo $fruit;
+}
+
+// Associative Array
+$person = array(
+"name" => "John", 
+"age" => 30,
+"city" => "New York");
+echo $person["name"];
+
+//foreach loop
+foreach ($person as $key => $value) {
+    echo "<br>";
+    echo $key.":".$value;
+}
+
+
+
+
+//Ex
+ $mark = array(
+         "kyaw" => 80,
+         "aung" => 40,
+         "Hla" => 70,
+ );
+
+foreach ($mark as $key => $value){
+    if ($value >= 50) {
+     echo "<br>";
+     echo $key . " passed";
+    } else {
+     echo "<br>";
+     echo $key . " failed";
+    }
+}
+?>
+
+<!-- defult valeue -->
+<!-- type restriction -->
+<!-- scope (local, global) -->
+
+<!-- -->
+
+
+
+
+   <!-- Project Start -->
+    <!-- PO - Product Owner -->
+    <!-- BA - Business Analyst -->
+    <!-- PM - Project Manager -->
+    <!-- QA - Quality Assurance -->
+    <!-- FE - Front End Developer -->
+    <!-- BE - Back End Developer -->
---
+++ form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>   
+        <div>
+            <label for="Age">Age:</label>
+            <input type="Age" id="Age" name="Age" required>
+        </div>
+        <div>
+            <button> Submit</button>
+        </div>
+    
+    </form>
+</body>
+  
+</html>
